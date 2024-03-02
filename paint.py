1152b4c5d1629b9e53fa1cda13813d5dc04e8265
CARVALHO Alisson <acarvalho5@elisbrasil.net>
2024-03-02 16:57:22 -0300

    Add comments

--- paint.py
+++ paint.py
@@ -3,22 +3,22 @@ import time
 
 distance = 300
 
-print(pyautogui.size())
-print(pyautogui.position())
+print(pyautogui.size()) #To know the size screen
+print(pyautogui.position())#To know the position of the mouse
 
 pyautogui.moveTo(667,1051)
 pyautogui.click(667,1051)
-pyautogui.write("paint")
+pyautogui.write("paint") #Typing the word "Paint"
 
 time.sleep(2)
 
-pyautogui.press("enter")
+pyautogui.press("enter")# Pressing the key
 
 time.sleep(2)
 
 pyautogui.moveTo(675,395)
 
-while distance > 0:
+while distance > 0: #Drawning in the paint
     pyautogui.dragRel(distance,0,1,button="left")
     distance = distance -20
     pyautogui.dragRel(0,distance,1,button="left")
